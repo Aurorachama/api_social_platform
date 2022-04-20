5924b470a813ef495e58b86168546b073e357215
Aurorachama <ronaldwong39@gmail.com>
2022-04-21 01:46:13 +0930

    user model created

---
+++ model/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+const assignmentSchema = require('./Assignment');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      max_letngth: 50,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'The email address is invalid, please try again']
+    },
+
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought",
+        }
+    ],
+
+    friends: [
+        {
+            type:Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    // id: false,
+  }
+);
+
+userSchema.virtual("friendCount")
+    .get(function () {
+        return (this.friends.length) ? (this.friends.length) : (0);
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
