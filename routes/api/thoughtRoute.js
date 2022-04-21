a9e0dc873360c50bd238a6f135ecf5b4e062a136
Aurorachama <ronaldwong39@gmail.com>
2022-04-21 21:12:27 +0930

    thought route created

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { getThoughts, getSingleThought, createThought, updateThought, deleteThought, addReaction, deleteReaction } = require("../../controllers/thoughtController");
+
+router.get("/", getThoughts);
+
+router.post("/", createThought);
+
+router.get("/:thoughtId", getSingleThought);
+
+router.put("/:thoughtId", updateThought);
+
+router.delete("/:thoughtId", deleteThought);
+
+router.post("/:thoughtId/reactions", addReaction);
+
+router.delete("/:thoughtId/reactions", deleteReaction);
+
+module.exports = router;
